46dee8115e6cd38f7c48aac67243e10c22a01af4
Dimas <dimas2018rplrus2@gmail.com>
2020-08-22 21:34:39 +0700

    Register And Dasboard

---
+++ app/src/main/java/com/example/dimas12rpl022018/RegisterLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.dimas12rpl022018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
