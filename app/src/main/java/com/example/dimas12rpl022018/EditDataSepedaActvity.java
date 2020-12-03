b41abde322cb59135910b5709561cd5ceaf4f267
DarkOktan <dimas2018rplrus2@gmail.com>
2020-12-03 09:09:51 +0700

    Add Insert and Update Data

---
+++ app/src/main/java/com/example/dimas12rpl022018/EditDataSepedaActvity.java
@@ -0,0 +1,14 @@
+package com.example.dimas12rpl022018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditDataSepedaActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_data_sepeda);
+    }
+}
